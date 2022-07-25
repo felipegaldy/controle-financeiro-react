699c742193e94fae53bcb3ec7d53b043893cf953
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-25 18:59:58 -0300

    criando estilo global

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components"; // eslint-disable-line
+
+const Global = createGlobalStyle`
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+        background-color: #222;
+        color: #fff;
+    }
+`
+export default Global;
