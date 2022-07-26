5c42c6d20cfe6f7f6f187cec3e2300cbc3be1be8
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-25 22:40:37 -0300

    adicionando header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import Global from './styles/global';
 
 const App = () => {
   return (
     <>
+      <Header />
       <Global />
       <h1>Hello World</h1>
     </>
---
+++ src/components/Header/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 1120px;
+  padding: 0 20px;
+  height: 150px;
+  background: #fff;
+  box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.1);
+`;
+
+export const Header = styled.h1 ``;
+
+
